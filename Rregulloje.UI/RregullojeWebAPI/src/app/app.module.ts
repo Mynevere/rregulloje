82ea8abace31b912de56266c5cf01e2bc367d765
Mynevere <56651074+Mynevere@users.noreply.github.com>
2020-09-07 10:17:12 +0200

    adding home-page, contact-us, footer components

--- Rregulloje.UI/RregullojeWebAPI/src/app/app.module.ts
+++ Rregulloje.UI/RregullojeWebAPI/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    ContactUsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
