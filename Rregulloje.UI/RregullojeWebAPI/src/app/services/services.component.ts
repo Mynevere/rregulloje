16d6ede553b8f5c6c5ebf90e55c521079c57497e
Mynevere <56651074+Mynevere@users.noreply.github.com>
2020-10-15 16:48:21 +0200

    card animation -not final result

--- Rregulloje.UI/RregullojeWebAPI/src/app/services/services.component.ts
+++ Rregulloje.UI/RregullojeWebAPI/src/app/services/services.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../_Services/service.service';
 import { ServiceViewModel } from '../_ViewModels/ServiceViewModel';
@@ -5,10 +6,24 @@ import { ServiceViewModel } from '../_ViewModels/ServiceViewModel';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179.9deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class ServicesComponent implements OnInit {
   companyServices: ServiceViewModel[];
+  flip: string = 'inactive';
+  flipped: number;
 
   constructor(private serviceService: ServiceService) { }
 
@@ -18,7 +33,21 @@ export class ServicesComponent implements OnInit {
 
   getServices() {
     this.serviceService.getServices().subscribe(response => {
-      this.companyServices = response;
+      this.companyServices = response
     })
+  }
+
+  toggleFlip(i: number) {
+
+    if (i === this.flipped) {
+      this.flipped = null;
+    } else {
+      this.flipped = i;
+    }
+  }
+
+  isFlipped(i: number) {
+    console.log(this.flipped)
+    return this.flipped === i ? 'active' : 'inactive';
   }
 }
