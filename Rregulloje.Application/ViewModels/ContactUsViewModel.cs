6ab9ad7ad0c5700fc1c8272fcf36f2d084740f7d
Mynevere <56651074+Mynevere@users.noreply.github.com>
2020-10-16 14:15:30 +0200

    front fixes

---
+++ Rregulloje.Application/ViewModels/ContactUsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rregulloje.Application.ViewModels
+{
+    public class ContactUsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
